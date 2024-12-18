340f4fe2bf3c48a69c688923244f28136d06c3d7
Sana Khemir <archi.khemirsana@gmail.com>
2024-12-18 19:47:09 +0100

    changement Network Requests in JavaScript

--- app.js
+++ app.js
@@ -1,34 +1,109 @@
-const button = document.querySelector('#btn')
 
-console.log(button)
+const { response } = require("express");
 
-button.addEventListener('click' , function(){
-    console.log('clicked')
-    getWeather()
-})
 
-function getWeather(){
-    const city = document.getElementById('city').value
-
-    console.log('affichage fuction get weather', city)
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "f29db718496190f279ef569a9aaa2182";
 
 
 
+weatherForm.addEventListener("submit", event =>{
+
+    event.preventDefault();
+
+    const city = cityInput.Value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData (city);
+            displayWeatherInfo(weatherData)
+
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+
+        }
+
+    }
+    else{
+        displayError("Please enter a city");
+    }
+
+});
+
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const reponse = await fetch(apiUrl);
+
+    if (!response.ok){
+        throw new Error("Could net fetch weather data");
+    }
+    return await response.json()
+
+}
+
+function displayWeatherInfo(data){
+
+   const {name: city, 
+         main: {temp, humidity}, 
+         weather: [{description, id }]} = data;
+
+    card.textContent = ""  ;
+    card.Style.display ="flex";   
+
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const getWeatherEmoji = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${((temp - 273.15) * (9/5) + 32).toFixed(1) }°C`;
+    humidityDisplay.textContent = `Humidity: ${(humidity)}%`;
+    descDisplay.textContent = description;
+    getWeatherEmoji.textContent = getWeatherEmoji(id)
+
+
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay")
+    humidityDisplay.classList.add("humidityDisplay")
+    descDisplay.classList.add("descDisplay")
+    getWeatherEmoji.classList.add("weatherEmoji")
+
+
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild( weatherEmoji );
+}
+
+function getWeatherEmoji(weatherId){
+
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return ""
+    }
     
-const apiKey =  'fe824cb856a37281c8c13e7a5fbbd488' ;
-const url = 'https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}'
-
-try{
-
-    const reponse=  await fetch(url)
-    const data = await reponse.json()
-    console.log('Succcess Api:', data)
-
-}
-catch (error) {
-    console.error('error from cath block')
-    
 }
 
-}
+function displayError(message){
 
+    const errorDisplay = document.createElement("p")
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent =" ";
+    card.computedStyleMap.display = "flex";
+    card.appendChild(errorDisplay);
+
+
+
+}
